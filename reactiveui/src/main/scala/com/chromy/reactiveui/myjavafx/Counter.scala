85d33c199492d8895075a8089d8d8853b7ffd907
Gabor Chromy <chromygabor@gmail.com>
2015-07-03 17:20:34 +0200

    update

--- reactiveui/src/main/scala/com/chromy/reactiveui/myjavafx/Counter.scala
+++ reactiveui/src/main/scala/com/chromy/reactiveui/myjavafx/Counter.scala
@@ -5,7 +5,8 @@ import javafx.scene.control.{Button, Label}
 
 import com.chromy.reactiveui.Dispatcher.DispatcherFactory
 import com.chromy.reactiveui.Utils._
-import com.chromy.reactiveui.myjavafx.Counter.{ActualState, Close, Decrement, Increment}
+import com.chromy.reactiveui.myjavafx.Action
+import com.chromy.reactiveui.myjavafx.Counter.{Close, Decrement, Increment}
 import com.chromy.reactiveui.myjavafx.CounterApp.Nop
 import rx.lang.scala.schedulers.ComputationScheduler
 import rx.lang.scala.{Observable, Observer, Subject, Subscriber}
@@ -16,19 +17,69 @@ import scala.util.{Failure, Success, Try}
 case class CounterModel(uid: String = Uid.nextUid().toString, value: Int = 0, buttonEnabled: Boolean = true)
 
 case class CounterDispatcher(parentFactory: DispatcherFactory[CounterModel, Action],
-                             private val actionsChannel: Observer[Action], changes: Observable[CounterModel],
-                             private val getSubscriber: (Observer[Action]) => Subscriber[CounterModel],
-                             initialState: CounterModel = CounterModel()) {
-  //  private val parent = parentFactory({ action => State[CounterModel, Action] { model => Counter.upd(action, model) -> action } })
+                             protected val channel: Observer[Action], changes: Observable[CounterModel],
+                             protected val getSubscriber: (Observer[Action]) => Subscriber[CounterModel],
+                             initialState: CounterModel = CounterModel(),
+                             update: (Action, CounterModel, Observer[Action]) => CounterModel = Counter.upd ) extends SimpleDispatcher[CounterModel]
+
+object SimpleDispatcher {
+  type State = { def uid: String}
+
+}
+
+trait SimpleDispatcher[T <: SimpleDispatcher.State] {
+  val parentFactory: DispatcherFactory[T, Action]
+  protected val channel: Observer[Action]
+  val changes: Observable[T]
+  protected val getSubscriber: (Observer[Action]) => Subscriber[T]
+  val initialState: T
+  def update: (Action, T, Observer[Action]) => T
+
+  private lazy val name =  s"DSP-${this.getClass.getSimpleName}(${initialState.uid})"
+  println(s"[$name] created ")
+
+  case class ActualState(action: Action, state: T) extends ActionWrapper
+  case class Step(action:Action, state: T) extends Action
 
   val actions = Subject[Action]
 
-  val subscriber = getSubscriber(actions)
+  val render = Subject[T]
+
+  lazy val subscriber = new Subscriber[T]() {
+    val dest = getSubscriber(actions)
+    override def onNext(value: T): Unit = {
+      println(s"[$name] rendering: $value ")
+      dest.onNext(value)
+    }
+
+    override def onError(error: Throwable): Unit = {
+      dest.onError(error)
+    }
+
+    override def onCompleted(): Unit = {
+      dest.onCompleted()
+    }
+  }
+
 
   val stream = actions.observeOn(ComputationScheduler()).scan((initialState, initialState, Nop.asInstanceOf[Action])) { case ((beforePrevState, prevState, prevAction), action) =>
-    Try {
-      val newState = Counter.upd(action, prevState, actionsChannel)
-      newState
+    Try[T] {
+      action match {
+        case Step(action, newState) =>
+          newState
+        case StateChange(actionToWrap, actualState: T) =>
+          println(s"=================== [$name] action received  $action =================")
+          println(s"[$name] action is ActualState $actualState")
+          actualState
+        case action: ActionWrapper =>
+          println(s"[$name] action is already a WrapperAction so return the previous state")
+          prevState
+        case action =>
+          println(s"=================== [$name] action received  $action =================")
+          val newState = update(action, prevState, actions)
+          println(s"[$name] action triggered a new state: $prevState => $newState")
+          newState
+      }
     } match {
       case Success(newState) => (prevState, newState, action)
       case Failure(error) =>
@@ -38,44 +89,67 @@ case class CounterDispatcher(parentFactory: DispatcherFactory[CounterModel, Acti
   }
 
   stream.drop(1).observeOn(JavaFXScheduler()).distinctUntilChanged.subscribe({ input =>
-    println(s"input: $input")
     input match {
       case (prevState, actualState, action) =>
         action match {
-          case e: LocalAction => subscriber.onNext(actualState)
-          case e => actionsChannel.onNext(ActualState(e, actualState))
+          case Step(_, _) =>
+            println(s"[$name] action is a step so nothing to do: $action")
+          case e: LocalAction =>
+            println(s"[$name] action is LocalAction triggering render: $actualState")
+            subscriber.onNext(actualState)
+          case action@StateChange(a, s: T) =>
+            val wrap = ActualState(a,s)
+            println(s"[$name] action is StateChange sending to channel as a $wrap")
+            channel.onNext(wrap)
+          case action: ActionWrapper =>
+            println(s"[$name] action is already a WrapperAction so just send up to channel: ${action}")
+            channel.onNext(action)
+          case action =>
+            println(s"[$name] action is a simple action wrapping to ActualState and sending to channel: ${ActualState(action, actualState)}")
+            channel.onNext(ActualState(action, actualState))
         }
 
     }
   })
 
-  private val parent = parentFactory.subscribe { (prevState, action) =>
-    println(s"subscriber update: $action")
+  val parent = parentFactory.subscribe { (prevState, action) =>
+    println(s"[$name] a new state was requested for $prevState and $action")
+
     action match {
-      case ActualState(_, actualState) => actualState
-      case e: ActionWrapper => Counter.upd(e.action, prevState, actions)
-      case e: Action => Counter.upd(e, prevState, actions)
+      case ActualState(_, actualState: T) if(actualState.uid == initialState.uid) =>
+        println(s"[$name] action is ActualState so we just unwrap it => $actualState")
+        actualState
+      case e: ActionWrapper =>
+        val newState = update(e.action, prevState, actions)
+        println(s"[$name] action is ActionWrapper so unwrap it and call update => $newState")
+        actions.onNext(Step(e.action, newState))
+        newState
+      case e: Action =>
+        val newState = update(e, prevState, actions)
+        println(s"[$name] action is a simple action so we call update => $newState")
+        newState
+      case _ =>
+        println(s"[$name] something went wrong, we return the prevState: $prevState")
+        prevState
     }
   }
 
-
-//  changes.distinctUntilChanged.subscribe({state =>
-//    println(s"changes received: $state")
-//  })
+  def init() = render.onNext(initialState)
 
   changes.distinctUntilChanged.subscribe(subscriber)
+
 }
+
+case class StateChange[T](action: Action, state: T) extends ActionWrapper
 
 object Counter extends GenericModule[CounterModel, CounterDispatcher] {
   import scala.concurrent.ExecutionContext.Implicits.global
 
   case class Increment(id: String) extends LocalAction
 
-  case class Decrement(id: String) extends LocalAction
+  case class Decrement(id: String) extends Action
 
-  case class Close(id: String) extends LocalAction
-
-  case class ActualState(action: Action, state: CounterModel) extends ActionWrapper
+  case class Close(id: String) extends Action
 
   def upd(action: Action, model: CounterModel, actionsChannel: Observer[Action]): CounterModel = action match {
     case Close(model.uid) =>
@@ -83,11 +157,11 @@ object Counter extends GenericModule[CounterModel, CounterDispatcher] {
     case Increment(model.uid) =>
       Future {
         println("Working....")
-        Thread.sleep(5000)
+        Thread.sleep(3000)
         println("Ready....")
         model.copy(value = model.value + 1)
       } map { newState =>
-        actionsChannel.onNext(ActualState(Nop, newState))
+        actionsChannel.onNext(StateChange(action, newState))
       }
       model.copy(buttonEnabled = false)
     case Decrement(model.uid) =>
@@ -98,7 +172,7 @@ object Counter extends GenericModule[CounterModel, CounterDispatcher] {
 //        actionsChannel.onNext(ActualState(Nop, newState))
 //      }
 
-      model.copy(buttonEnabled = false)
+      model.copy(value = model.value - 1)
 
     case _ => model
   }
@@ -120,12 +194,10 @@ class Counter extends GenericJavaFXModule[Counter.type] {
     new Subscriber[CounterModel] {
       override def onNext(model: CounterModel): Unit = {
 
-        println(s"render: $model")
-
         btnIncrement.setDisable(!model.buttonEnabled)
         btnDecrement.setDisable(!model.buttonEnabled)
 
-        lblCounter.setText(model.value.toString)
+        lblCounter.setText(s"${model.uid} - ${model.value.toString}" )
         btnIncrement.setOnAction { () => actions.onNext(Increment(model.uid)) }
         btnDecrement.setOnAction { () => actions.onNext(Decrement(model.uid)) }
         btnClose.setOnAction { () => actions.onNext(Close(model.uid)) }
