bb24acd219fbe3d4ad2b8e6a071cb00db80f5dea
Gabor Chromy <chromygabor@gmail.com>
2015-08-11 00:06:33 +0200

    Update CaseClassTrav.scala

--- sandbox/src/main/scala/CaseClassTrav.scala
+++ sandbox/src/main/scala/CaseClassTrav.scala
@@ -1,85 +1,65 @@
+//compile 'org.scala-lang:scala-reflect:2.11.6'
+import scala.reflect.runtime.universe._
+import reflect.runtime.{currentMirror=>mirror}
+
 /**
  * Created by chrogab on 2015.07.07..
  */
 object CaseClassTrav extends App {
   trait Component
-  trait ComponentModel {
-    type Component
-  }
-  trait Model[C <: Component] extends ComponentModel {
+  trait Model[C <: Component] {
     type Component = C
   }
 
-  class MainComponent extends Component {
+  class MainComponent(param: Int) extends Component {
     def dummyMain() = {}
   }
-  class SubComponent extends Component {
+  class SubComponent(param: Int) extends Component {
+    println(s"param: $param")
     def dummySub() = {}
   }
 
   case class MainModel(left: SubModel = SubModel(), right: SubModel = SubModel()) extends Model[MainComponent]
   case class SubModel(value: Int = 0) extends Model[SubComponent]
 
-
-
-  def create[A <: ComponentModel with Product](implicit m1: Manifest[A], m2: Manifest[A#Component]): A#Component = {
-    //def isModel[T](implicit ev: T <:< ComponentModel = null) = ev != null
-    import scala.reflect.runtime.universe._
-    import reflect.runtime.{currentMirror=>mirror}
-
-    def isModel[M](t: Type): Boolean = {
-      t.baseClasses.contains(typeOf[ComponentModel].typeSymbol)
-    }
-
-    def createComponent(rdType: Type): AnyRef = {
-//      val values = Map("intVal"->5, "stringVal"->"five")
-//      val rdClass = mirror.reflectClass(rdType.typeSymbol.asClass)
-//      val constructors = rdType.members.filter(m=>m.isMethod && m.asMethod.isConstructor)
-//      val defaultCtor = rdType.member(nme.CONSTRUCTOR)
-//      val runtimeCtor = rdClass.reflectConstructor(defaultCtor.asMethod)
-//
-//      val paramsList = defaultCtor.asMethod.paramss
-//
-//      val mappedValues = params map (m=>values(m.name.toString))
-//      val newRD = runtimeCtor.apply(mappedValues:_*)
-
-      val rdClass = mirror.reflectClass(rdType.typeSymbol.asClass).symbol
-      val ctor = rdClass.primaryConstructor
-
-      val classMirror = mirror.reflectClass(rdType.typeSymbol.asClass)
-      classMirror.reflectConstructor(ctor.asMethod).apply(2, "bar")
-
-      ???
-    }
-
-    def echo(in: Type): Unit = {
-      in.baseClasses.map { bc =>
-        println(bc.typeSignature.members.filter(_.isType).filter(_.asType.isAliasType).map(_.typeSignature))
-      }
-    }
-
-    val l = typeOf[A].members.filter(!_.isMethod).map(_.typeSignature).filter(isModel).map { in =>
-      //createComponent(in)
-      echo(in)
-    }
-
-    //println(l)
-
-//    manifest[A].runtimeClass.getDeclaredFields.foreach { field =>
-//      //println(field.getType.getInterfaces)
-//
-//      //      val l = field.getType.getInterfaces.toList.foreach { in =>
-//      //        println(s" **** ${in.getName} ")
-//      //      }
-//      println(s"${field.getName} - ${field.getType} " )
-//    }
-
-
-    val comp = manifest[A#Component].runtimeClass.newInstance().asInstanceOf[A#Component]
-    comp
+  trait ComponentFactory {
+    def apply(param: Int): Component
   }
 
-  val s = create[MainModel]
-  s.dummyMain()
+  def isModel(t: Type): Boolean = {
+    t.baseClasses.contains(typeOf[Model[_]].typeSymbol)
+  }
 
+  def traverseModel(tModel: Type): Map[String, ComponentFactory] = {
+    tModel.members.filter(!_.isMethod).map(in => in.name ->  in.typeSignature).filter(in => isModel(in._2)).map { in =>
+      in._1.toString -> createComponentFactoryForModel(in._2)
+    }.toMap
+  }
+
+  def createComponentFactoryForModel(tModel: Type): ComponentFactory = {
+    val modelClass = typeOf[Model[_]].typeSymbol.asClass
+    val modelType = modelClass.typeParams(0).asType.toType
+    val rdType = modelType.asSeenFrom(tModel, modelClass)
+    val ctor = rdType.typeSymbol.asClass.primaryConstructor
+
+    val classMirror = mirror.reflectClass(rdType.typeSymbol.asClass)
+    new ComponentFactory {
+      override def apply(param: Int): Component =
+        classMirror.reflectConstructor(ctor.asMethod).apply(param).asInstanceOf[Component]
+    }
+  }
+
+  //println(traverseModel(typeOf[MainModel]))
+
+  case object MyTestObject
+
+  private def companionOf(companionName: String): Any = {
+    import scala.reflect.runtime.{currentMirror => cm}
+
+    val moduleSymbol = cm.staticModule(companionName)
+    val moduleMirror = cm.reflectModule(moduleSymbol)
+    moduleMirror.instance
+  }
+
+  println(companionOf("CaseClassTrav.MyTestObject"))
 }
