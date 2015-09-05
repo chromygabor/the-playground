58fdd1c64d0592ef62d6f0659fdbcf81b2104ede
Gabor Chromy <chromygabor@gmail.com>
2015-09-05 11:07:38 +0200

    Update Counter.scala

--- reactiveui/src/main/scala/com/chromy/reactiveui/myjavafx/Counter.scala
+++ reactiveui/src/main/scala/com/chromy/reactiveui/myjavafx/Counter.scala
@@ -5,27 +5,44 @@ import javafx.scene.control.{Button, Label}
 
 import com.chromy.reactiveui.core._
 import com.chromy.reactiveui.core.misc.Utils._
+import com.chromy.reactiveui.myjavafx.CounterApp.Repository
 import rx.lang.scala.{Subscription, Observer, Subscriber}
+
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Failure, Success}
 
 
 case class CounterModel(value: Int = 0, buttonEnabled: Boolean = true, uid: Uid = Uid()) extends Model[Counter]
 
 class Counter(protected val routerMapper: RouterMapper[CounterModel], protected val initialState: CounterModel) extends Component[CounterModel] {
-  override def update: (Action, ModelType, Observer[Action]) => ModelType = { (action, model, _) =>
-    action match {
-      case Close(model.uid) =>
-        model
-      case Increment(model.uid) =>
-        model.copy(value = model.value + 1)
-      case Decrement(model.uid) =>
-        model.copy(value = model.value - 1)
-      case _ => model
-    }
-  }
+  val counterStore = Repository.service[CounterStore]
 
+  override def upd(model: ModelType): PartialFunction[Action, ModelType] = {
+    case Close(model.uid) =>
+      model
+    case Increment(model.uid) =>
+      counterStore.increment(model.uid)
+
+      fut {
+        println("Computing....")
+        Thread.sleep(2000)
+        println("Ready")
+        1
+      } {
+        case (Success(result), model) => model.copy(value = model.value + result)
+        case (Failure(error), model) => model
+      }
+      model
+    case Incremented(model.uid) =>
+      model.copy(value = model.value + 1)
+    case Decrement(model.uid) =>
+      model.copy(value = model.value - 1)
+  }
   override def toString = s"Counter(${initialState.uid})"
+
 }
 
+case class Incremented(id: Uid) extends Action
 case class Increment(id: Uid) extends Action
 
 case class Decrement(id: Uid) extends Action
@@ -46,7 +63,7 @@ class CounterController extends GenericJavaFXModule[Counter] {
 
   def subscriber(channel: Observer[Action]): Subscriber[CounterModel] = new Subscriber[CounterModel]() {
     override def onNext(model: CounterModel): Unit = {
-      println("Counter render: " + model)
+      println(s"Counter render: $model on: ${Thread.currentThread.getName}" )
       btnIncrement.setDisable(!model.buttonEnabled)
       btnDecrement.setDisable(!model.buttonEnabled)
 
