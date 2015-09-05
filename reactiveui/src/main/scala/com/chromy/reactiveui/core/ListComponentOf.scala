4b025995e14f0cd24005ae3e6ceefa867dfad51f
Gabor Chromy <chromygabor@gmail.com>
2015-09-05 11:04:38 +0200

    Update ListComponentOf.scala

--- reactiveui/src/main/scala/com/chromy/reactiveui/core/ListComponentOf.scala
+++ reactiveui/src/main/scala/com/chromy/reactiveui/core/ListComponentOf.scala
@@ -2,13 +2,12 @@ package com.chromy.reactiveui.core
 
 
 import com.chromy.reactiveui.core.misc.ListDiff
-import rx.lang.scala.{Subscriber, Observable, Observer, Subject}
+import rx.lang.scala.{Scheduler => ScalaScheduler, _}
+import rx.schedulers.Schedulers
 
 /**
  * Created by cry on 2015.08.04..
  */
-
-case class Changes[A](deleteItems: List[A], insertItems: List[A], uid: Uid = Uid()) extends BaseModel
 
 object ListComponentOf {
   def apply[A <: BaseComponent{type ModelType <: BaseModel}](iRouterMapper: RouterMapper[List[A#ModelType]])(iCreate: (Router[A#ModelType], A#ModelType) => A): ListComponentOf[A] = {
@@ -48,32 +47,42 @@ trait ListComponentOf[A <: BaseComponent {type ModelType <: BaseModel}] extends 
   def childrenComponents_=(newComponents: Map[Uid, ComponentType]) = _components = newComponents
 
 
-  val myRouter = routerMapper { (action, original, state) =>
+  private val subscriber: (Action, List[ComponentModel], List[ComponentModel]) => List[ComponentModel] = { (action, original, state) =>
     println(s"[$name] a new state was requested for $original and $action")
 
-    val (toDelete, toInsert) = ListDiff.diff(original, state) {
+    val (_toDelete, _toInsert) = ListDiff.diff(original, state) {
       _.uid.uid.toString
     }
 
-    val deleteItems = toDelete.foldLeft((0, List[(ComponentModel, Int)]())) { case ((removedItems, list), (item, index)) =>
-      val newItem = item -> (index - removedItems)
-      (removedItems + 1, newItem :: list)
-    }._2.map { case (item, index) =>
-      val component = childrenComponents(item.uid)
-      toInsert.find { case (model, _) => model.uid == item.uid} match {
-        case Some(_) =>
+    val (toDelete, toInsert) = (_toDelete.toIndexedSeq, _toInsert.toIndexedSeq)
+
+    val (_, list) = toDelete.foldLeft((0, List.empty[(ComponentModel, Int, Int, Option[Int])])) { case ((numberOfRemovedItems, items), (item, removedIndex)) =>
+      toInsert.find { case (itemInsert, insertedIndex) => itemInsert == item } match {
+        case Some((_, insertedIndex)) =>
+          val newItem = (item, removedIndex, removedIndex - numberOfRemovedItems, Some(insertedIndex))
+          (numberOfRemovedItems + 1, newItem :: items)
         case None =>
-          childrenComponents = childrenComponents - item.uid
+          val newItem = (item, removedIndex, removedIndex - numberOfRemovedItems, None)
+          (numberOfRemovedItems + 1, newItem :: items)
       }
-      DeleteItem(component, index)
     }
 
-    val insertItems = toInsert.map { case (item, index) =>
+    val deleteItems = list.map { case (item, removedIndex, computedIndex, oInsertedIndex) =>
+      val component = childrenComponents(item.uid)
+      oInsertedIndex match {
+        case Some(insertedIndex) =>
+          MoveItem(component, removedIndex, computedIndex, insertedIndex)
+        case _ =>
+          DeleteItem(component, removedIndex, computedIndex)
+      }
+    }
 
-      val component = childrenComponents.getOrElse(item.uid, {
-        println(s"[$name] creating component with: $item")
-        createComponent(item)
-      })
+    val insertItems = toInsert.filter { case (item, index) => toDelete.find { case (deletedItem, _) => deletedItem == item } match {
+      case Some(_) => false
+      case _ => true
+      }
+    }.map { case (item, index) =>
+      val component = childrenComponents.getOrElse(item.uid, createComponent(item))
       childrenComponents = childrenComponents + (item.uid -> component)
       AddItem(component, index)
     }.toList
@@ -88,6 +97,7 @@ trait ListComponentOf[A <: BaseComponent {type ModelType <: BaseModel}] extends 
     }
   }
 
+  val myRouter = routerMapper(subscriber)
 
   private val myChanges = myRouter.changes.map(_.map { in => in.uid -> in }.toMap)
 
@@ -96,6 +106,8 @@ trait ListComponentOf[A <: BaseComponent {type ModelType <: BaseModel}] extends 
       override val changes: Observable[ComponentModel] = myChanges.filter {_.contains(item.uid)} map { change => change(item.uid)}
       override val chain: UpdateChain[ComponentModel] = UpdateChain[ComponentModel]
       override val channel: Observer[Action] = myRouter.channel
+      override val chainExecutor = myRouter.chainExecutor
+      override val changesExecutor = myRouter.changesExecutor
     }, item)
   }
 
@@ -108,11 +120,23 @@ trait ListComponentOf[A <: BaseComponent {type ModelType <: BaseModel}] extends 
       }
 
       override def channel: Observer[Action] = myRouter.channel
+
+      override val chainExecutor = myRouter.chainExecutor
+      override val changesExecutor = myRouter.changesExecutor
     }
   }
 
+  lazy val chainScheduler = new ScalaScheduler {
+    val asJavaScheduler = Schedulers.from(router.chainExecutor)
+  }
+
+  lazy val changesScheduler = new ScalaScheduler {
+    val asJavaScheduler = Schedulers.from(router.changesExecutor)
+  }
+
+
   def subscribe(subscriber: Subscriber[List[Operation[ComponentType]]]) = {
-    router.changes.subscribe(subscriber)
+    router.changes.observeOn(changesScheduler).subscribe(subscriber)
   }
 }
 
@@ -120,6 +144,8 @@ sealed trait Operation[M] extends Model[ListComponentOf[_]] {
   override val uid = Uid()
 }
 
-case class DeleteItem[M](item: M, index: Int) extends Operation[M]
+case class DeleteItem[M](item: M, originalIndex: Int, computedIndex: Int) extends Operation[M]
 
 case class AddItem[M](item: M, index: Int) extends Operation[M]
+
+case class MoveItem[M](item: M, originalIndex: Int, computedIndex: Int, newIndex: Int) extends Operation[M]
