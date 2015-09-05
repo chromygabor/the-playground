1d24f89a5e28dd9ea59aad7abce46ba6a9112d5a
Gabor Chromy <chromygabor@gmail.com>
2015-09-05 11:10:06 +0200

    Update Router.scala

--- reactiveui/src/main/scala/com/chromy/reactiveui/core/Router.scala
+++ reactiveui/src/main/scala/com/chromy/reactiveui/core/Router.scala
@@ -1,14 +1,19 @@
 package com.chromy.reactiveui.core
 
+import java.util.concurrent.Executor
+
 import monocle.Lens
-import rx.lang.scala.{Observable, Observer}
+import rx.lang.scala.schedulers.ImmediateScheduler
+import rx.lang.scala.{Scheduler, Observable, Observer}
+import rx.schedulers.Schedulers
+
+import scala.concurrent.ExecutionContext
 
 /**
  * Created by cry on 2015.07.05..
  */
 
 trait RouterMapper[B] {
-  def apply(f: (Action, B) => B): Router[B]
   def apply(f: (Action, B, B) => B): Router[B]
 }
 
@@ -16,6 +21,9 @@ trait Router[A] {
   def changes: Observable[A]
   def channel: Observer[Action]
   def chain: UpdateChain[A]
+
+  def chainExecutor: Executor
+  def changesExecutor: Executor
 
   def map[B](lens: Lens[A, B]): RouterMapper[B] = {
     val parent = this
@@ -27,9 +35,11 @@ trait Router[A] {
         apply(realSubscriber)
       }
       def apply(f: (Action, B, B) => B): Router[B] = new Router[B] {
-        val changes = parent.changes.map { in => lens.get(in) }
-        val channel = parent.channel
-        val chain = parent.chain.fromLens(lens)
+        override val changes = parent.changes.map { in => lens.get(in) }
+        override val channel = parent.channel
+        override val chain = parent.chain.fromLens(lens)
+        override val chainExecutor: Executor = parent.chainExecutor
+        override val changesExecutor: Executor = parent.changesExecutor
         chain.subscribe(f)
       }
     }
@@ -38,10 +48,6 @@ trait Router[A] {
   def mapper: RouterMapper[A] = {
     val parent = this
     new RouterMapper[A] {
-      def apply(f: (Action, A) => A): Router[A] = {
-        parent.chain.subscribe(f)
-        parent
-      }
       def apply(f: (Action, A, A) => A): Router[A] = {
         parent.chain.subscribe(f)
         parent
