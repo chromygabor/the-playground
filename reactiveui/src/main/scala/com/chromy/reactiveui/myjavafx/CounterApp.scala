d29d887c1d7fbca3bde2436fdda5f5ce26df4321
Gabor Chromy <chromygabor@gmail.com>
2015-09-12 10:45:10 +0200

    Update CounterApp.scala

--- reactiveui/src/main/scala/com/chromy/reactiveui/myjavafx/CounterApp.scala
+++ reactiveui/src/main/scala/com/chromy/reactiveui/myjavafx/CounterApp.scala
@@ -1,6 +1,6 @@
 package com.chromy.reactiveui.myjavafx
 
-import java.util.concurrent.Executor
+import java.util.concurrent.{Executors, Executor}
 import javafx.application.Platform
 import javafx.embed.swing.JFXPanel
 import javafx.fxml.FXMLLoader
@@ -9,11 +9,12 @@ import javafx.stage.Stage
 
 import com.chromy.reactiveui.core._
 import monocle.macros.GenLens
-import rx.lang.scala.schedulers.ComputationScheduler
+import rx.lang.scala.schedulers.{IOScheduler, ComputationScheduler}
 import rx.lang.scala.subjects.BehaviorSubject
-import rx.lang.scala.{Scheduler => ScalaScheduler, Observer, Subject}
+import rx.lang.scala.{Scheduler => ScalaScheduler, Observable, Observer, Subject}
 import rx.schedulers.Schedulers
 
+import scala.annotation.implicitNotFound
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.{Try, Failure, Success}
 
@@ -104,7 +105,7 @@ trait GenericJavaFXModule[A <: BaseComponent] extends JavaFXModule {
 
 }
 
-case class CounterAppModel(counters: CountersModel = CountersModel(), counterStore: CounterStoreModel = CounterStoreModel())
+case class CounterAppModel(counters: CountersModel = CountersModel(), counterStore: CounterStoreModel = CounterStoreModel(), services: Map[String, _ <: BaseModel] = Map())
 
 object CounterApp extends App {
 
@@ -128,23 +129,18 @@ object CounterApp extends App {
     override val chain: UpdateChain[M] = UpdateChain()
     override val channel = Subject[Action]
 
-    override val chainExecutor = ExecutionContext.fromExecutor(null: Executor)
-    override val changesExecutor = JavaFXScheduler.executor
+    val chainSch = IOScheduler()
+    val changesSch = JavaFXScheduler()
+    override val backgroundExecutor: ExecutionContext = ExecutionContext.fromExecutor(Executors.newCachedThreadPool())
 
-    lazy val chainScheduler = new ScalaScheduler {
-      val asJavaScheduler = Schedulers.from(chainExecutor)
-    }
+    println(s"App initialstate: $initialState")
 
-    lazy val changesScheduler = new ScalaScheduler {
-      val asJavaScheduler = Schedulers.from(changesExecutor)
-    }
-
-
-    val stream = channel.subscribeOn(chainScheduler).scan(initialState) { (oldState, action) =>
+    val stream = channel.observeOn(chainSch).scan(initialState) { (oldState, action) =>
       Try {
-        println(s"=================== [$name] action received  $action =================")
+        println(s"=================== [$name] action received  $action ================= on ${Thread.currentThread.getName}")
+        println(s"[$name] - An action received in the main loop: $action, updating state $oldState...")
         val newState = chain.update(action, oldState)
-        println(s"[$name] - An action received in the main loop: $action -- $oldState => $newState")
+        println(s"[$name] - result is: $newState")
         newState
       } match {
         case Success(newState) => newState
@@ -154,10 +150,11 @@ object CounterApp extends App {
       }
     }
 
-    stream.drop(1).subscribeOn(changesScheduler).subscribe ({ newState =>
-      println(s"[$name] - A publishing a change: $newState")
+    stream.drop(1).observeOn(changesSch).subscribe({ newState =>
+      println(s"[$name] - A publishing a change: $newState, on: ${Thread.currentThread().getName}")
       changes.onNext(newState)
     })
+
   }
 
 
@@ -180,17 +177,54 @@ object CounterApp extends App {
     }
   })
 
+  @implicitNotFound("Not found ComponentBuilder in scope for ${A}")
+  trait ServiceBuilder[A <: BaseComponent] {
+    def init(context: ContextMapper[A#ModelType]): () => A
+  }
+
+  val serviceSubscriber: (Action, Map[String, _ <: BaseModel], Map[String, _ <: BaseModel]) => Map[String, _ <: BaseModel] = { (action, _, _ ) =>
+
+  }
+
+  val servicesContext = context.map(GenLens[CounterAppModel](_.services))
+  servicesContext(serviceSubscriber)
+
   object Repository {
     private[CounterApp] val services = scala.collection.mutable.HashMap.empty[String, BaseComponent]
+    private[CounterApp] val sers = scala.collection.mutable.HashMap.empty[String, () => _ <: BaseComponent]
 
-    def service[A : Manifest]: A = {
+    def ser[A <: BaseComponent : Manifest : ServiceBuilder]: A = {
+      val m = manifest[A]
+
+      val serviceName = m.runtimeClass.getName
+
+      sers.getOrElse(serviceName, {
+        val sb = implicitly[ServiceBuilder[A]]
+        val serviceContext = new Context[A#ModelType] {
+          override def changes: Observable[A#ModelType] = context.changes.map {_.services}.filter {_.contains(serviceName)}.map{ in => in(serviceName).asInstanceOf[A#ModelType]}
+
+          override def chain: UpdateChain[A#ModelType] = UpdateChain[A#ModelType]
+
+          override def channel: Observer[Action] = context.channel
+
+          override def backgroundExecutor: ExecutionContext = context.backgroundExecutor
+        }
+        sers(serviceName) = sb.init(serviceContext.mapper)
+        sers(serviceName)
+      }).apply().asInstanceOf[A]
+    }
+
+    def service[A: Manifest]: A = {
       val mf = manifest[A]
-      val service = services.find { case(name, service) =>
-        mf.runtimeClass.isInstance(service)
-      }
-      if(service.isEmpty) throw new IllegalStateException(s"Service not found by type: ${mf.runtimeClass.getName}")
 
-      service.asInstanceOf[A]
+      val foundService = for {
+        found <- services.find { case (name, service) => mf.runtimeClass.isInstance(service) }
+      } yield(found._2.asInstanceOf[A])
+
+      if (foundService.isEmpty) throw new IllegalStateException(s"Service not found by type: ${mf.runtimeClass.getName}")
+
+      foundService.get
     }
   }
+
 }
