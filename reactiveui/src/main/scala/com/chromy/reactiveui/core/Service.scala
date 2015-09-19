0099ad0ac96938af138da5efecfa9c96ebd2fb15
Gabor Chromy <chromygabor@gmail.com>
2015-09-19 09:08:12 +0200

    Update Service.scala

--- reactiveui/src/main/scala/com/chromy/reactiveui/core/Service.scala
+++ reactiveui/src/main/scala/com/chromy/reactiveui/core/Service.scala
@@ -3,5 +3,8 @@ package com.chromy.reactiveui.core
 /**
  * Created by cry on 2015.09.13..
  */
-case class Service[A <: BaseModel](store:  () => (Context[A], A)) {
+case class Service[A <: BaseComponent](name: String, context: Context[A#ModelType], val initialState: A#ModelType, service: A) {
+  def update(action: Action, model: ComponentModel): A#ModelType = {
+    context.chain.update(action, model.asInstanceOf[A#ModelType])
+  }
 }
