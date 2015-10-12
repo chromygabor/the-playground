b435629e28819a726fec34b2fa2b602e9a88689d
Gabor Chromy <chromygabor@gmail.com>
2015-10-12 17:02:25 +0200

    Update SideChain.scala

--- reactiveui/src/main/scala/com/chromy/reactiveui/core/misc/SideChain.scala
+++ reactiveui/src/main/scala/com/chromy/reactiveui/core/misc/SideChain.scala
@@ -10,8 +10,10 @@ import scala.collection.mutable.{WeakHashMap => WMap, Map => MMap}
 
 trait Executable {
   def run(): Unit
+
   def error(): Unit = {}
 }
+
 object Executable {
   def apply(f: => Unit) = new Executable {
     override def run(): Unit = f
@@ -19,16 +21,21 @@ object Executable {
 
   def apply(success: => Unit, unsuccess: => Unit) = new Executable {
     override def run(): Unit = success
+
     override def error(): Unit = unsuccess
   }
+}
+
+trait BehaviorSideChain[T] extends SideChain[T] {
+  def subscribeAndApply(subscriber: (T) => Executable): Executable
 }
 
 trait SideChain[T] {
   private[this] val _subscribersLock: Object = new Object()
   private[this] val _subscribers: MMap[(T => Executable), Int] = WMap()
 
-  /*private[SideChain]*/ def subscribers: List[(T => Executable)] = {
-    var newSubscribers:List[T => Executable]  = null
+  private[SideChain] def subscribers: List[(T => Executable)] = {
+    var newSubscribers: List[T => Executable] = null
     _subscribersLock.synchronized {
       newSubscribers = _subscribers.toList.sortBy(_._2).map {
         _._1
@@ -37,17 +44,40 @@ trait SideChain[T] {
     newSubscribers
   }
 
-  def filter(pred: T => Boolean): SideChain[T] = {
+  def asBehavior(initialState: T): BehaviorSideChain[T] = {
     val myParent = this
-    new SideChain[T] {
-      val parent = myParent
+    new BehaviorSideChain[T] {
+      private val parent = myParent
+      private val lastItem = new AtomicReference[T](initialState)
+
       override val update: T => Executable = { in =>
-        if(pred(in)) {
+        if (lastItem.getAndSet(in) != in) {
           val allSubscribers = subscribers.map(_.apply(in))
           Executable {
             allSubscribers.foreach(_.run())
           }
-        } else Executable{}
+        } else Executable {}
+      }
+      parent.subscribe(update)
+
+      override def subscribeAndApply(subscriber: (T) => Executable): Executable = {
+        subscribe(subscriber)
+        subscriber.apply(lastItem.get)
+      }
+    }
+  }
+
+  def filter(pred: T => Boolean): SideChain[T] = {
+    val myParent = this
+    new SideChain[T] {
+      private val parent = myParent
+      override val update: T => Executable = { in =>
+        if (pred(in)) {
+          val allSubscribers = subscribers.map(_.apply(in))
+          Executable {
+            allSubscribers.foreach(_.run())
+          }
+        } else Executable {}
       }
       parent.subscribe(update)
     }
@@ -56,16 +86,16 @@ trait SideChain[T] {
   def distinctUntilChanged: SideChain[T] = {
     val myParent = this
     new SideChain[T] {
-      val lastItem = new AtomicReference[T]()
-      val parent = myParent
+      private val lastItem = new AtomicReference[T]()
+      private val parent = myParent
 
       override val update: T => Executable = { in =>
-        if(lastItem.getAndSet(in) != in) {
+        if (lastItem.getAndSet(in) != in) {
           val allSubscribers = subscribers.map(_.apply(in))
           Executable {
             allSubscribers.foreach(_.run())
           }
-        } else Executable{}
+        } else Executable {}
       }
       parent.subscribe(update)
     }
@@ -75,7 +105,7 @@ trait SideChain[T] {
   def map[B](f: T => B): SideChain[B] = {
     val myParent = this
     new SideChain[B] {
-      val parent = myParent
+      private val parent = myParent
       override val update: B => Executable = { in =>
         val allSubscribers = subscribers.map(_.apply(in))
         Executable {
@@ -90,17 +120,6 @@ trait SideChain[T] {
     }
   }
 
-//  def to[B](lens: Lens[T,B]): SideChain[B] = {
-//    val parent = SideChain.this
-//    new SideChain[B] {
-//      private val updater: T => () => Unit = { input =>
-//        val newInput = lens.get(input)
-//        this.update(newInput)
-//      }
-//      parent.subscribe(updater)
-//    }
-//  }
-
   val update: T => Executable
 
   def subscribe(subscriber: (T) => Executable): Unit = {
@@ -112,22 +131,12 @@ trait SideChain[T] {
 }
 
 object SideChain {
-  def apply[T]: SideChain[T] = new SideChain[T] {
-    override val update: T => Executable = { in =>
-      val allSubscribers = subscribers.map ( _.apply(in) )
+  def apply[T](): SideChain[T] = new SideChain[T] {
+    override val update: T => Executable =  { in =>
+      val allSubscribers = subscribers.map(_.apply(in))
       Executable {
         allSubscribers.foreach(_.run())
       }
     }
   }
-
-  def behavior[T]: SideChain[T] = new SideChain[T] {
-    override val update: T => Executable = { in =>
-      val allSubscribers = subscribers.map ( _.apply(in) )
-      Executable {
-        allSubscribers.foreach(_.run())
-      }
-    }
-  }
-
 }
