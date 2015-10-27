5234d565dd36ff76ff2f92b43a4608b10715f93d
Gabor Chromy <chromygabor@gmail.com>
2015-10-27 17:56:12 +0100

    Update Model.scala

--- frpui/src/main/scala/com/chromy/frpui/core/Model.scala
+++ frpui/src/main/scala/com/chromy/frpui/core/Model.scala
@@ -7,22 +7,28 @@ import monocle._
  * Created by cry on 2015.10.18..
  */
 
+trait Context {
+  def getService[B <: Model[B] : Manifest](serviceName: String): B
+  def onAction(action: Action): Unit
+}
+
+
 case class Child[M, B](lens: Lens[M, B]) {
-  private[this] def updateSeq[B](action: Action, list: Seq[B]): Seq[B] = {
+  private[this] def updateSeq[B](action: Action, list: Seq[B])(implicit context: Context): Seq[B] = {
     list.map {
       case e: BaseModel => e.step(action).asInstanceOf[B]
       case e => e
     }
   }
 
-  private[this] def updateMap[K, B](action: Action, map: Map[K, B]): Map[K, B] = {
+  private[this] def updateMap[K, B](action: Action, map: Map[K, B])(implicit context: Context): Map[K, B] = {
     map.map {
       case (key, value: BaseModel) => (key, value.step(action).asInstanceOf[B])
       case e => e
     }
   }
 
-  def step(action: Action, model: M): M = {
+  def step(action: Action, model: M)(implicit context: Context): M = {
     val newModel = lens.get(model) match {
       case m: BaseModel =>
         m.step(action)
@@ -46,8 +52,9 @@ object Updater {
 }
 
 
+
 object BaseModel {
-  def step[A <: BaseModel](action: Action, model: A, children: List[Child[A, _]], handle: Updater[A]): A = {
+  def step[A <: BaseModel](action: Action, model: A, children: List[Child[A, _]], handle: Updater[A])(implicit context: Context): A = {
     val handler = handle.handle
     val initialModel: A = if (handler.isDefinedAt(action)) handler.apply(action) else model.asInstanceOf[A]
     val newModel = children.foldLeft(initialModel) { (model, child) =>
@@ -64,11 +71,12 @@ trait BaseModel {
 
   def children: List[Child[M, _]] = Nil
 
-  def step(action: Action): M = BaseModel.step[M](action, this.asInstanceOf[M], children, handle)
+  def step(action: Action)(implicit context: Context): M = BaseModel.step[M](action, this.asInstanceOf[M], children, handle)
 
   val handle: Updater[M] = Simple { case _ => this.asInstanceOf[M] }
 }
 
 trait Model[B <: BaseModel] extends BaseModel {
   type M = B
+  implicit lazy val model: B = this.asInstanceOf[B]
 }
