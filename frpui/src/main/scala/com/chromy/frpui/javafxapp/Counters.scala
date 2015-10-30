8002cfa8136556680c911631a6c90e99cafb9b32
Gabor Chromy <chromygabor@gmail.com>
2015-10-30 17:17:37 +0100

    Update Counters.scala

--- frpui/src/main/scala/com/chromy/frpui/javafxapp/Counters.scala
+++ frpui/src/main/scala/com/chromy/frpui/javafxapp/Counters.scala
@@ -1,8 +1,14 @@
 package com.chromy.frpui.javafxapp
 
+import com.chromy.frpui.core._
+
 /**
  * Created by cry on 2015.10.30..
  */
-class Counters {
+case class Counters(val uid: Uid = Uid()) extends Model[Counters]{
+  override protected def handle(implicit context: Context): Updater[Counters] = ???
+}
 
+class CountersController extends Controller[Counters] {
+  override val subscriber: (Counters) => SideEffect = ???
 }
