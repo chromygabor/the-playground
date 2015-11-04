41645ce6dc4ea7b0bde6b9fa91407c2989f44932
Gabor Chromy <chromygabor@gmail.com>
2015-11-04 18:32:18 +0100

    Update Counter.scala

--- frpui/src/main/scala/com/chromy/frpui/javafxapp/Counter.scala
+++ frpui/src/main/scala/com/chromy/frpui/javafxapp/Counter.scala
@@ -1,19 +1,69 @@
 package com.chromy.frpui.javafxapp
 
+import javafx.fxml.FXML
+import javafx.scene.control.{Button, Label}
+
 import com.chromy.frpui.Renderer
-import com.chromy.frpui.fw.core.{Render, SideEffect, Model, Uid}
+import com.chromy.frpui.fw.core._
 import com.chromy.frpui.fw.javafx.Controller
+import com.chromy.frpui.fw.javafx.Utils._
 
 /**
  * Created by cry on 2015.10.30..
  */
-case class Counter(uid: Uid = Uid()) extends Model[Counter] {
+
+trait Counter extends Model[Counter] {
+  val value: Int
+}
+
+case class ExistingCounter(value: Int, uid: Uid = Uid()) extends Counter
+
+case class NotExistingCounter(value: Int, uid: Uid = Uid()) extends Counter
+
+object Counter extends Behavior[Counter] {
+  def increment = Action { (_, model) =>
+    model match {
+      case model: ExistingCounter => model.copy(model.value + 1)
+      case _ => model
+    }
+  }
+
+  def decrement = Action { (_, model) =>
+    model match {
+      case model: ExistingCounter => model.copy(model.value - 1)
+      case _ => model
+    }
+  }
+
+  def close = Action { (_, model) =>
+    NotExistingCounter(0, model.uid)
+  }
+
+  def apply() = ExistingCounter(0)
 }
 
 class CounterController extends Controller[Counter] {
-//  override def subscriber: (Counter) => SideEffect = Render { model =>
-//    SideEffect()
-//  }
+  @FXML private var _lblCounter: Label = _
+  @FXML private var _btnIncrement: Button = _
+  @FXML private var _btnDecrement: Button = _
+  @FXML private var _btnClose: Button = _
+
+  lazy val lblCounter = _lblCounter
+  lazy val btnIncrement = _btnIncrement
+  lazy val btnDecrement = _btnDecrement
+  lazy val btnClose = _btnClose
 
   override lazy val renderer: Renderer[Counter] = Renderer()
+    Renderer { implicit model =>
+      SideEffect {
+        lblCounter.setText(model.value.toString)
+
+        btnIncrement.setOnAction(() => fire(Counter.increment))
+
+        btnDecrement.setOnAction(() => fire(Counter.decrement))
+
+        btnClose.setOnAction(() => fire(Counter.close))
+      }
+    }
+  
 }
