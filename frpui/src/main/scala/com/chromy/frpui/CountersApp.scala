65a60ef54d51515b858a644fbdb2d7d3e2df0982
Gabor Chromy <chromygabor@gmail.com>
2015-11-17 18:34:07 +0100

    Update CountersApp.scala

--- frpui/src/main/scala/com/chromy/frpui/CountersApp.scala
+++ frpui/src/main/scala/com/chromy/frpui/CountersApp.scala
@@ -1,18 +1,14 @@
 package com.chromy.frpui.javafxapp
 
-import javafx.application.Platform
 import javafx.embed.swing.JFXPanel
 import javafx.scene.Scene
 import javafx.stage.Stage
 
-import com.chromy.frpui.{Renderer, ConfigServiceImpl}
 import com.chromy.frpui.fw.core._
-import com.chromy.frpui.fw.javafx.{Controller, JavaFX, JavaFXScheduler}
+import com.chromy.frpui.fw.javafx.{JavaFX, JavaFXScheduler}
 import rx.lang.scala.{Scheduler => ScalaScheduler}
 
 import scala.util.{Failure, Success}
-
-
 
 
 object CountersApp extends App {
@@ -23,38 +19,18 @@ object CountersApp extends App {
   )
 
   val initialState = Counters()
-  val app = FrpApp[Counters](services = services, state = initialState, sideEffectScheduler = JavaFXScheduler())
-
-  class App extends Controller[Unit] {
-    override protected val renderer = Renderer { in =>
-      SideEffect {
-        JavaFX[CountersController](context, renderer, app.initialState) match {
-          case Success((parent, _, effect)) =>  //We don't need to hold reference to controller, because the parent holds it
-            val stage = new Stage
-            stage.setScene(new Scene(parent))
-            stage.setTitle("CounterPair App")
-            stage.show()
-            effect.run()
-          case Failure(e) =>
-            e.printStackTrace()
-        }
+  val app = JavaFxApp(state = initialState, services = services, sideEffectScheduler = JavaFXScheduler()) { (context, render, initialState) =>
+    SideEffect {
+      JavaFX[CountersController](context, render, initialState) match {
+        case Success((parent, _, effect)) => //We don't need to hold reference to controller, because the parent holds it
+          val stage = new Stage
+          stage.setScene(new Scene(parent))
+          stage.setTitle("CounterPair App")
+          stage.show()
+          effect.run()
+        case Failure(e) =>
+          e.printStackTrace()
       }
     }
   }
-  
-//  Platform.runLater(new Runnable() {
-//    override def run(): Unit = {
-//      JavaFX[CountersController](???, app.render, app.initialState) match {
-//        case Success((parent, _, effect)) =>  //We don't need to hold reference to controller, because the parent holds it
-//          val stage = new Stage
-//          stage.setScene(new Scene(parent))
-//          stage.setTitle("CounterPair App")
-//          stage.show()
-//          effect.run()
-//        case Failure(e) =>
-//          e.printStackTrace()
-//      }
-//
-//    }
-//  })
 }
