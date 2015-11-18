cdf4a451e4654f260099d872af0e8b1f71e956dd
Gabor Chromy <chromygabor@gmail.com>
2015-11-18 18:30:33 +0100

    Update JavaFX.scala

--- frpui/src/main/scala/com/chromy/frpui/fw/javafx/JavaFX.scala
+++ frpui/src/main/scala/com/chromy/frpui/fw/javafx/JavaFX.scala
@@ -3,7 +3,8 @@ package com.chromy.frpui.fw.javafx
 import javafx.fxml.FXMLLoader
 import javafx.scene.Parent
 
-import com.chromy.frpui.fw.core.{Context, SideEffect, Event, SideEffectChain}
+import com.chromy.frpui.RendererChain.RendererChain
+import com.chromy.frpui.fw.core.{RendererContext, SideEffect}
 
 import scala.util.Try
 
@@ -11,7 +12,7 @@ import scala.util.Try
  * Created by cry on 2015.11.01..
  */
 object JavaFX {
-  def apply[M <: BaseController : Manifest](context: Context, render: SideEffectChain[M#C], initialState: M#C): Try[(Parent, M, SideEffect)] = {
+  def apply[M <: BaseController : Manifest](context: RendererContext, render: RendererChain[M#C], initialState: M#C): Try[(Parent, M, SideEffect)] = {
     val ct = manifest[M]
 
     Try {
@@ -22,54 +23,10 @@ object JavaFX {
       val node: Parent = loader.load()
       val controller = loader.getController[M]
 
-      val sideEffect = controller.init(context, render.asInstanceOf[SideEffectChain[controller.C]], initialState.asInstanceOf[controller.C])
-
+      val sideEffect = controller.init(context, render.asInstanceOf[RendererChain[controller.C]], initialState.asInstanceOf[controller.C])
       
-      //      sideEffect.errors.foreach(_.printStackTrace())
-
       (node, controller, sideEffect)
     }
 
   }
-
-  //  def apply[M <: JavaFXController : Manifest](component: M#Component): Try[(Parent, M, M#Component)] = {
-  //    val ct = manifest[M]
-  //
-  //    Try {
-  //      val clazzName = if (ct.runtimeClass.getSimpleName.endsWith("$")) ct.runtimeClass.getSimpleName.dropRight(1) else ct.runtimeClass.getSimpleName
-  //
-  //      val loader = new FXMLLoader(ct.runtimeClass.getResource(s"$clazzName.fxml"))
-  //
-  //      val node: Parent = loader.load()
-  //      val controller = loader.getController[M]
-  //      controller.dispatch(component.asInstanceOf[controller.Component])
-  //      //component.context.changes.update(component.context.initialState).run()
-  //      (node, controller, component)
-  //    }
-  //  }
-  //
-  //  def apply[M <: JavaFXController : Manifest](contextMapper: ContextMapper[M#Model]): Try[(Parent, M, M#Component)] = {
-  //    import reflect.runtime.{currentMirror => mirror}
-  //
-  //    val ct = manifest[M]
-  //
-  //    scala.util.Try {
-  //      val clazzName = if (ct.runtimeClass.getSimpleName.endsWith("$")) ct.runtimeClass.getSimpleName.dropRight(1) else ct.runtimeClass.getSimpleName
-  //      val loader = new FXMLLoader(ct.runtimeClass.getResource(s"$clazzName.fxml"))
-  //
-  //      val typeOfComponent = JavaFXController.componentType[M]
-  //
-  //      val ctor = typeOfComponent.typeSymbol.asClass.primaryConstructor
-  //
-  //      val classMirror = mirror.reflectClass(typeOfComponent.typeSymbol.asClass)
-  //      val component = classMirror.reflectConstructor(ctor.asMethod).apply(contextMapper).asInstanceOf[M#Component]
-  //
-  //      val node: Parent = loader.load()
-  //      val controller = loader.getController[M]
-  //      controller.dispatch(component.asInstanceOf[controller.Component])
-  //      //component.context.changes.update(component.context.initialState).run()
-  //      (node, controller, component)
-  //    }
-  //  }
-
 }
