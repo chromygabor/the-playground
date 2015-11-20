b06af55e5f96c67bdd9425e8087dd39cf0928c0c
Gabor Chromy <chromygabor@gmail.com>
2015-11-20 17:18:51 +0100

    Update Model.scala

--- frpui/src/main/scala/com/chromy/frpui/fw/core/Model.scala
+++ frpui/src/main/scala/com/chromy/frpui/fw/core/Model.scala
@@ -10,37 +10,37 @@ case object Init extends Event
 
 case class Targeted(target: Uid, action: Event) extends Event
 
-trait UpdateAction[M] extends Event {
+trait Command[M] extends Event {
   def uid: Uid
-  def apply(context: Context, model: M): M
-  
+
+  def apply(context: UpdateContext, model: M): Unit
 }
-trait PostOrderAction 
-trait PreOrderAction 
+
+trait PostOrderAction
+
+trait PreOrderAction
 
 
 trait Initializable
 
 case class Child[M, B](lens: Lens[M, B]) {
 
-  private[this] def updateSeq[B](action: Event, prevList: Seq[B], list: Seq[B])(implicit context: Context): Seq[B] = {
+  private[this] def updateSeq[B](action: Event, prevList: Seq[B], list: Seq[B])(implicit context: UpdateContext): Seq[B] = {
     val prevSet = prevList.toSet
 
-    list.map { input =>
-      input match {
-        case e: BaseModel with Initializable if (!prevSet(input)) =>
-          e.step(Init).step(action).asInstanceOf[B]
-        case e: BaseModel => e.step(action).asInstanceOf[B]
-        case e => e
-      }
-    }
+    list.map(input => input match {
+      case e: BaseModel with Initializable if !prevSet(input) =>
+        e.step(Init).step(action).asInstanceOf[B]
+      case e: BaseModel => e.step(action).asInstanceOf[B]
+      case e => e
+    })
   }
 
-  private[this] def updateMap[K, B](action: Event, prevMap: Map[K, B], map: Map[K, B])(implicit context: Context): Map[K, B] = {
+  private[this] def updateMap[K, B](action: Event, prevMap: Map[K, B], map: Map[K, B])(implicit context: UpdateContext): Map[K, B] = {
     val prevSet = prevMap.values.toSet
-    map.map { case(key, item) => 
+    map.map { case (key, item) =>
       item match {
-        case (value: BaseModel with Initializable) if(!prevSet(item)) => 
+        case (value: BaseModel with Initializable) if (!prevSet(item)) =>
           (key, value.step(Init).step(action).asInstanceOf[B])
         case (value: BaseModel) => (key, value.step(action).asInstanceOf[B])
         case e => (key, e)
@@ -48,12 +48,12 @@ case class Child[M, B](lens: Lens[M, B]) {
     }
   }
 
-  def step(action: Event, previousModel: M, model: M)(implicit context: Context): M = {
+  def step(action: Event, previousModel: M, model: M)(implicit context: UpdateContext): M = {
     val newModel = lens.get(model) match {
       case m: BaseModel =>
         action match {
           case Targeted(m.uid, action) => m.step(action)
-          case _ => m.step(action) 
+          case _ => m.step(action)
         }
       case s: Seq[B] =>
         val ps = lens.get(previousModel).asInstanceOf[s.type]
@@ -69,8 +69,7 @@ case class Child[M, B](lens: Lens[M, B]) {
 }
 
 object BaseModel {
-  def step[A <: BaseModel](action: Event, model: A, children: List[Child[A, _]], handle: EventHandler[A])(implicit context: Context): A = {
-    println(s"Stepping $model for $action")
+  def step[A <: BaseModel](action: Event, model: A, children: List[Child[A, _]], handle: EventHandler[A])(implicit context: UpdateContext): A = {
     action match {
       case _ =>
         val previousModel = model
@@ -81,19 +80,27 @@ object BaseModel {
               child.step(action, previousModel, model)
             }
             action match {
-              case d: UpdateAction[_] if d.uid == model.uid =>
-                val defer = d.asInstanceOf[UpdateAction[A]]
-                defer(context, model)
+              case d: Command[_] if d.uid == model.uid =>
+                val command = d.asInstanceOf[Command[A]]
+                command.apply(context, model)
+                model
+              case _: Command[_] =>
+                model
               case _ =>
+                println(s"Stepping $model for $action")
                 val handler = steppedModel.handle.handle
                 if (handler.isDefinedAt(e)) handler.apply(e) else steppedModel.asInstanceOf[A]
             }
           case _ =>
             val initialModel: A = action match {
-              case d: UpdateAction[_] if d.uid == model.uid =>
-                val defer = d.asInstanceOf[UpdateAction[A]]
-                defer(context, model)
+              case d: Command[_] if d.uid == model.uid =>
+                val command = d.asInstanceOf[Command[A]]
+                command.apply(context, model)
+                model
+              case _: Command[_] =>
+                model
               case _ =>
+                println(s"Stepping $model for $action")
                 val handler = handle.handle
                 if (handler.isDefinedAt(action)) handler.apply(action) else model.asInstanceOf[A]
             }
@@ -113,11 +120,9 @@ trait BaseModel {
 
   protected def children: List[Child[M, _]] = Nil
 
-  final def step(action: Event)(implicit context: Context): M = BaseModel.step[M](action, this.asInstanceOf[M], children, handle(context))
+  final def step(action: Event)(implicit context: UpdateContext): M = BaseModel.step[M](action, this.asInstanceOf[M], children, handle(context))
 
-  protected[core] def handle(implicit context: Context): EventHandler[M] = EventHandler()
-
-  //protected def action(f: (Context, M) => M): DelayedEvent[M] = DelayedEvent[M](uid, f)
+  protected[core] def handle(implicit context: UpdateContext): EventHandler[M] = EventHandler()
 }
 
 trait Model[B <: BaseModel] extends BaseModel with Initializable {
