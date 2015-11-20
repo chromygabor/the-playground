fa424b9265081f33efa046899e7811729d1cc11c
Gabor Chromy <chromygabor@gmail.com>
2015-11-20 17:18:02 +0100

    Update Contexts.scala

--- frpui/src/main/scala/com/chromy/frpui/fw/core/Contexts.scala
+++ frpui/src/main/scala/com/chromy/frpui/fw/core/Contexts.scala
@@ -6,50 +6,43 @@ import com.chromy.frpui.Renderer
  * Created by chrogab on 2015.11.18..
  */
 
-trait Context {
-  def getService[B : Manifest]: B
+//************ FrpApp contexts
 
+trait UpdateContext {
+  def getService[B : Manifest]: B
   def onAction(action: Event): Unit
 }
 
-trait ModelContext extends Context {
-  
+trait RenderContext {
+  def updateContext: UpdateContext
+  def subscribeToService[B <: BaseService: Manifest](renderer: Renderer[B, RenderContext]): Unit
 }
 
-object ModelContext {
-  //def apply()
-}
 
+//******** Other contexts
 trait BehaviorContext {
   def getService[B : Manifest]: B
   def onAction(action: Event): Unit
 }
 
 object BehaviorContext {
-  def apply(context: Context, uid: Uid): BehaviorContext = new BehaviorContext {
+  def apply(context: UpdateContext, uid: Uid): BehaviorContext = new BehaviorContext {
     override def getService[B: Manifest]: B = context.getService[B]
 
-    override def onAction(action: Event): Unit =
-      action match {
-        case e: BehaviorAction[_] => context.onAction(e(uid))
-        case e => context.onAction(e)
-      }
+    override def onAction(action: Event): Unit = ???
   }
 }
 
-trait RendererContext {
-  def context: Context
-  def subscribeToService[B <: BaseService: Manifest](renderer: Renderer[B, RendererContext]): Unit
-}
-
-trait ControllerContext[M <: BaseModel] extends RendererContext {
-  def command(f: (BehaviorContext, M) => Unit): Unit
+trait ControllerContext[M <: BaseModel] extends RenderContext {
+  def call(f: (UpdateContext, Uid) => Unit): Unit
 }
 
 object ControllerContext {
-  def apply[M <: BaseModel](context: RendererContext, model: M) : ControllerContext[M] = new ControllerContext[M] {
-    override def command(f: (BehaviorContext, M) => Unit): Unit = ??? //f.apply(context, model)
+  def apply[M <: BaseModel](context: RenderContext, model: M) : ControllerContext[M] = new ControllerContext[M] {
+    override def call(f: (UpdateContext, Uid) => Unit): Unit = f.apply(updateContext, model.uid)
 
-    override def subscribeToService[B <: BaseService: Manifest](renderer: Renderer[B, RendererContext]): Unit = context.subscribeToService(renderer)
-  }
+    override def updateContext: UpdateContext = context.updateContext
+
+    override def subscribeToService[B <: BaseService : Manifest](renderer: Renderer[B, RenderContext]): Unit = ???
+  } 
 }
