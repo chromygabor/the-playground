3ff22c5b05713c5df26391c6391037594ae372f4
Gabor Chromy <chromygabor@gmail.com>
2015-11-20 17:18:34 +0100

    Update Behavior.scala

--- frpui/src/main/scala/com/chromy/frpui/fw/core/Behavior.scala
+++ frpui/src/main/scala/com/chromy/frpui/fw/core/Behavior.scala
@@ -4,53 +4,55 @@ package com.chromy.frpui.fw.core
  * Created by cry on 2015.11.01..
  */
 
-trait BehaviorAction[A <: BaseModel]  extends Event {
-  def apply(uid: Uid): UpdateAction[A]// = new UpdateAction(uid, { (context, model) => apply(BehaviorContext(context, uid), model) })
-}
 
 trait Behavior[M <: BaseModel] {
-  import com.chromy.frpui.fw.core.{PostOrderAction => PostA, PreOrderAction => PreA, UpdateAction}
+//  import com.chromy.frpui.fw.core.{PostOrderAction => PostA, PreOrderAction => PreA, UpdateAction}
+//  object Handler {
+//    def apply(iHandler: PartialFunction[Event, BehaviorAction[M]]): PartialFunction[Event, BehaviorAction[M]] = iHandler
+//  }
+//  object Action {
+//    def apply(action: (BehaviorContext, M) => M): BehaviorAction[M] = new BehaviorAction[M] {
+//      override def apply(iUid: Uid): UpdateAction[M] = new UpdateAction[M] with PreA {
+//        override def uid: Uid = iUid
+//
+//        override def apply(context: AppContext, model: M): M = action(BehaviorContext(context, iUid), model)
+//      }
+//    }
+//  }
+//
+//  object PostOrderAction {
+//    def apply(action: (BehaviorContext, M) => M): BehaviorAction[M] = new BehaviorAction[M] {
+//      override def apply(iUid: Uid): UpdateAction[M] = new UpdateAction[M] with PostA {
+//        override def uid: Uid = iUid
+//        override def apply(context: AppContext, model: M): M = action(BehaviorContext(context, iUid), model)
+//      }
+//    }
+//  }
+//
+//  def eventHandler(model: M)(implicit context: AppContext): EventHandler[M] = new EventHandler[M] {
+//    override val handle: PartialFunction[Event, M] = {
+//      case e if handler(model).isDefinedAt(e) =>
+//        val updateAction = handler(model)(e).apply(model.uid)
+//        updateAction.apply(BehaviorContext(context, model.uid), model)
+//    }
+//  }
+//  
+//  def handler(model: M): PartialFunction[Event, BehaviorAction[M]] = new PartialFunction[Event, BehaviorAction[M]] {
+//    override def isDefinedAt(x: Event): Boolean = false
+//    override def apply(v1: Event): BehaviorAction[M] = ???
+//  }
 
-  object Handler {
-    def apply(iHandler: PartialFunction[Event, BehaviorAction[M]]): PartialFunction[Event, BehaviorAction[M]] = iHandler
+  def action(f: (BehaviorContext, M) => M): ((UpdateContext, Uid) => Unit) = new ((UpdateContext, Uid) => Unit) {
+    override def apply(context: UpdateContext, uid: Uid): Unit = ???
   }
   
-  
-  object Action {
-    def apply(action: (BehaviorContext, M) => M): BehaviorAction[M] = new BehaviorAction[M] {
-      override def apply(iUid: Uid): UpdateAction[M] = new UpdateAction[M] with PreA {
+  def command(f: (BehaviorContext, M) => Unit): ((UpdateContext, Uid) => Unit) = new ((UpdateContext, Uid) => Unit) {
+    override def apply(context: UpdateContext, iUid: Uid): Unit = {
+      val command = new Command[M] {
         override def uid: Uid = iUid
-
-        override def apply(context: Context, model: M): M = action(BehaviorContext(context, iUid), model)
+        override def apply(context: UpdateContext, model: M): Unit = f(BehaviorContext(context, model.uid), model)
       }
-    }
-  }
-
-  object PostOrderAction {
-    def apply(action: (BehaviorContext, M) => M): BehaviorAction[M] = new BehaviorAction[M] {
-      override def apply(iUid: Uid): UpdateAction[M] = new UpdateAction[M] with PostA {
-        override def uid: Uid = iUid
-        override def apply(context: Context, model: M): M = action(BehaviorContext(context, iUid), model)
-      }
-    }
-  }
-
-  def eventHandler(model: M)(implicit context: Context): EventHandler[M] = new EventHandler[M] {
-    override val handle: PartialFunction[Event, M] = {
-      case e if handler(model).isDefinedAt(e) =>
-        val updateAction = handler(model)(e).apply(model.uid)
-        updateAction.apply(BehaviorContext(context, model.uid), model)
-    }
-  }
-  
-  def handler(model: M): PartialFunction[Event, BehaviorAction[M]] = new PartialFunction[Event, BehaviorAction[M]] {
-    override def isDefinedAt(x: Event): Boolean = false
-    override def apply(v1: Event): BehaviorAction[M] = ???
-  }
-
-  def command(f: (BehaviorContext, M) => Unit): ((Context, M) => Unit) = new ((Context, M) => Unit) {
-    override def apply(context: Context, model: M): Unit = {
-      f(BehaviorContext(context, model.uid), model) 
+      context.onAction(command)      
     }
   }
 }
