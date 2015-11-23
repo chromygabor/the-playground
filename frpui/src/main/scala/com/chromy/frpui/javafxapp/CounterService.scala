bf37dfb78542143e2661971b770f5f85f55ca487
Gabor Chromy <chromygabor@gmail.com>
2015-11-23 17:24:14 +0100

    Update CounterService.scala

--- frpui/src/main/scala/com/chromy/frpui/javafxapp/CounterService.scala
+++ frpui/src/main/scala/com/chromy/frpui/javafxapp/CounterService.scala
@@ -3,8 +3,6 @@ package com.chromy.frpui.javafxapp
 import com.chromy.frpui.fw.core._
 
 import scala.collection.immutable.ListMap
-import scala.concurrent.Future
-import scala.reflect.ClassTag
 
 /**
  * Created by cry on 2015.11.06..
@@ -12,122 +10,21 @@ import scala.reflect.ClassTag
 trait CounterService {
   def addCounter(): Command[_ <: Service[CounterService, _]]
 
-  def subscribe(uid: Uid): Unit
-
   def increment(uid: Uid): Command[_ <: Service[CounterService, _]]
   
-  def decrement(uid: Uid): Unit
-  
-  def unsubscribe(uid: Uid): Unit
+  def decrement(uid: Uid): Command[_ <: Service[CounterService, _]]
+
+  def close(sUid: Uid): Command[_ <: Service[CounterService, _]]
+
 }
-
-trait CounterState {
-  val value: Int
-  val hasPrev: Boolean
-  val hasNext: Boolean
-  val index: Int
-
-  def next: Option[(Uid, CounterState)]
-
-  def prev: Option[(Uid, CounterState)]
-}
-
-object CounterState {
-  def apply(iValue: Int, iIndex: Int, iHasNext: Boolean, iHasPrev: Boolean, iNext: => Option[(Uid, CounterState)], iPrev: => Option[(Uid, CounterState)]): CounterState = new CounterState {
-    override val value: Int = iValue
-    override val hasPrev: Boolean = iHasPrev
-    override val hasNext: Boolean = iHasNext
-    override val index: Int = iIndex
-
-    override def prev: Option[(Uid, CounterState)] = iPrev
-
-    override def next: Option[(Uid, CounterState)] = iNext
-  }
-}
-
-trait CounterStateAccessor {
-  def firstAvailable(actual: Option[(Uid, CounterState)]): Option[(Uid, CounterState)]
-
-  def apply(uid: Uid): Option[(Uid, CounterState)]
-}
-
-case class CountersChanged(f: CounterStateAccessor) extends Event
 
 
 object CounterServiceImpl extends Behavior[CounterServiceImpl] {
-//  def subscribe(uid: Uid) = Action { (context, model) =>
-//    val newModel = model.copy(counters = model.counters.updated(uid, 0))
-//    modelChanged(context, newModel)
-//    newModel
-//  }
-//
-//  def unsubscribe(uid: Uid) = Action { (context, model) =>
-//    val newModel = model.copy(counters = model.counters - uid)
-//    newModel
-//  }
-//
-//  def decrement(uid: Uid) = Action { (context, model) =>
-//    val newModel = model.copy(counters = model.counters.updated(uid, model.counters.getOrElse(uid, 0) - 1))
-//    modelChanged(context, newModel)
-//    newModel
-//  }
-//
-//  def increment(uid: Uid) = Action { (context, model) =>
-//    val newModel =  model.copy(counters = model.counters.updated(uid, model.counters.getOrElse(uid, 0) + 1))
-//    modelChanged(context, newModel)
-//    newModel
-//  }
-//
-//  private[this] def modelChanged(context: BehaviorContext, newModel: CounterServiceImpl): Unit = {
-//    context.onAction(CountersChanged(stateAccessor(newModel.counters)))
-//  }
-//
-  def stateAccessor(counters: ListMap[Uid, Int]): CounterStateAccessor = {
-    val indexedCounters = counters.zipWithIndex.map { case ((uid, value), index) => uid ->(value, index) } toMap
-
-    def createState(iUid: Uid): Option[(Uid, CounterState)] = for {
-      (_, (value, index)) <- indexedCounters.find { case (uid, (value, index)) => uid == iUid }
-    } yield {
-
-        val oNextUid = indexedCounters.find { case (uid, (_, currIndex)) => currIndex == index + 1 }
-        lazy val nextState = oNextUid.flatMap { case (uid, (_, _)) => createState(uid) }
-        val oPrevUid = indexedCounters.find { case (uid, (_, currIndex)) => currIndex == index - 1 }
-        lazy val prevState = oPrevUid.flatMap { case (uid, (_, _)) => createState(uid) }
-
-        (iUid, CounterState(value,
-          index,
-          oNextUid.isDefined,
-          oPrevUid.isDefined,
-          nextState,
-          prevState
-        ))
-      }
-
-    new CounterStateAccessor {
-      override def firstAvailable(actual: Option[(Uid, CounterState)]): Option[(Uid, CounterState)] = actual match {
-        case None =>
-          for {
-            (uid, (_, _)) <- indexedCounters.find { case (_, (_, index)) => index == 0 }
-            counterState <- createState(uid)
-          } yield counterState
-        case Some((_, counterState)) =>
-          (for {
-            (uid, (_, _)) <- indexedCounters.find { case (_, (_, index)) => index == counterState.index - 1  }
-            newCounterState <- createState(uid)
-          } yield newCounterState) match {
-            case s: Some[_] => s
-            case None => for {
-              (uid, (_, _)) <- indexedCounters.find { case (_, (_, index)) => index == 0 }
-              counterState <- createState(uid)
-            } yield counterState
-          }
-      }
-
-      override def apply(uid: Uid): Option[(Uid, CounterState)] = createState(uid)
-    }
-  }
   
 }
+
+case class CounterAdded(counterUid: Uid, value: Int) extends Event
+case class CounterChanged(counterUid: Uid, value: Int) extends Event
 
 /**
  * Add a new counter
@@ -138,7 +35,7 @@ case class AddCounter(counterUid: Uid) extends Command[CounterServiceImpl] {
     model.copy(counters = model.counters.updated(counterUid, 0))
   
   def notification(context: UpdateContext, newModel: CounterServiceImpl): Event = 
-    CountersChanged(CounterServiceImpl.stateAccessor(newModel.counters))
+    CounterAdded(counterUid, newModel.counters(counterUid))
 }
 
 /**
@@ -149,9 +46,21 @@ case class IncrementCounter(counterUid: Uid) extends Command[CounterServiceImpl]
   def apply(context: UpdateContext, model: CounterServiceImpl): CounterServiceImpl =
     model.copy(counters = model.counters.updated(counterUid, model.counters.getOrElse(counterUid, 0) + 1))
 
+  def notification(context: UpdateContext, newModel: CounterServiceImpl): Event =
+    CounterChanged(counterUid, newModel.counters(counterUid))
+}
+
+/**
+ * Decrement a counter
+ * @param counterUid: Uid of the counter to add
+ */
+case class DecrementCounter(counterUid: Uid) extends Command[CounterServiceImpl] {
+  def apply(context: UpdateContext, model: CounterServiceImpl): CounterServiceImpl =
+    model.copy(counters = model.counters.updated(counterUid, model.counters.getOrElse(counterUid, 0) - 1))
+
 
   def notification(context: UpdateContext, newModel: CounterServiceImpl): Event =
-    CountersChanged(CounterServiceImpl.stateAccessor(newModel.counters))
+    CounterChanged(counterUid, newModel.counters(counterUid))
 }
 
 case class CounterServiceImpl(uid: Uid = Uid(), counters: ListMap[Uid, Int] = ListMap()) extends Service[CounterService, CounterServiceImpl] {
@@ -162,16 +71,10 @@ case class CounterServiceImpl(uid: Uid = Uid(), counters: ListMap[Uid, Int] = Li
   }
 
   override def api(context: UpdateContext): CounterService = new CounterService {
-    override def subscribe(sUid: Uid): Unit = ??? //context.onAction(CounterServiceImpl.subscribe(sUid)(uid))
 
-    override def decrement(sUid: Uid): Unit = ??? //context.onAction(CounterServiceImpl.decrement(sUid)(uid))
-
-
-    override def unsubscribe(sUid: Uid): Unit = ??? //context.onAction(CounterServiceImpl.unsubscribe(sUid)(uid))
-
-    override def increment(sUid: Uid) = {
-      IncrementCounter(sUid)
-    } 
+    override def decrement(sUid: Uid) = DecrementCounter(sUid)
+    override def increment(sUid: Uid) = IncrementCounter(sUid)
+    override def close(sUid: Uid) = ???
     
     override def addCounter() =  {
       println("Sleeping")
