15cfad9eeebd34c6aaca9ae30907b98a84e2c1f5
Gabor Chromy <chromygabor@gmail.com>
2015-11-23 17:22:57 +0100

    Update Uid.scala

--- frpui/src/main/scala/com/chromy/frpui/fw/core/Uid.scala
+++ frpui/src/main/scala/com/chromy/frpui/fw/core/Uid.scala
@@ -2,12 +2,14 @@ package com.chromy.frpui.fw.core
 
 import java.util.concurrent.atomic.AtomicInteger
 
+import scala.util.Random
+
 /**
  * Created by cry on 2015.08.04..
  */
 object Uid {
-  val _nextUid = new AtomicInteger(0)
-  private[core] def next(): Int = _nextUid.getAndIncrement()
+  val _nextUid = new AtomicInteger(Random.nextInt(100000))
+  private[core] def next(): String = _nextUid.getAndSet(Random.nextInt(100000)).toString
 }
 
-case class Uid(uid: Int = Uid.next())
+case class Uid(uid: String = Uid.next())
