4da21712bd33fee4c902bddb6072ead9c20bd7f1
Gabor Chromy <chromygabor@gmail.com>
2016-02-23 23:51:50 +0100

    InsertionSort

---
+++ sandbox/src/main/scala/algorythms/InsertionSort.scala
@@ -0,0 +1,18 @@
+package algorythms
+
+import org.scalatest.FunSpecLike
+
+/**
+ * Created by cry on 2016.02.23..
+ */
+object InsertionSort {
+  
+}
+
+class InsertionSortTest extends FunSpecLike {
+  describe("InsertionSort") {
+    it("should order") {
+      
+    }
+  }
+}
