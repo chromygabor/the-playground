507999a9db45d3c3a0b3aabfb1175d71da397a61
Gabor Chromy <chromygabor@gmail.com>
2016-10-15 10:51:02 +0200

    Update EventStreamActor.scala

--- ev-playground/src/main/scala/ui/EventStreamActor.scala
+++ ev-playground/src/main/scala/ui/EventStreamActor.scala
@@ -2,12 +2,17 @@ package ui
 
 import java.util.UUID
 
-import akka.actor.{Actor, ActorLogging, ActorRef, Terminated}
+import akka.actor.{ActorLogging, ActorRef, Terminated}
+import akka.persistence.{PersistentActor, RecoveryCompleted}
 
 /**
   * Created by Gábor on 2016.09.28..
   */
-class EventStreamActor extends Actor with ActorLogging {
+
+class EventStreamActor extends PersistentActor with ActorLogging {
+  override def persistenceId = "EventStream"
+
+  var lastSentId = 0L
 
   def loop(subscribers: List[ActorRef]): Receive = {
     case Subscribe(subscriber) =>
@@ -20,22 +25,49 @@ class EventStreamActor extends Actor with ActorLogging {
     case Unsubscribe(subscriber) =>
       log.debug(s"Unsubscribe from: $subscriber")
       context.become(loop(subscribers.filterNot(_ == subscriber)))
-    case event: EventEnvelope =>
-      log.debug(s"Received: $event")
-      subscribers.foreach(_ ! event)
+    case (sender: SenderId, e: Event) =>
+      val event = Envelope(sender, e, lastSentId + 1)
+      event.event match {
+        case e: PersistentEvent =>
+          persist(event) { event =>
+            log.debug(s"Event persisted: $event")
+            subscribers.foreach(_ ! event)
+          }
+        case e =>
+          log.info(s"Received: $event")
+          subscribers.foreach(_ ! event)
+      }
+      lastSentId = event.id
+
   }
 
-  override def receive: Actor.Receive = loop(Nil)
-}
-
-case class EventEnvelope(sender: ActorRef, event: Event) extends Event {
-  val uid = UUID.randomUUID().toString
-
-  override def toString(): String = {
-    s"EventEnvelope[$uid]($event, $sender)"
+  override def receiveRecover: Receive = {
+    case e: Envelope =>
+      lastSentId = e.id
+      log.debug(s"Change lastSentId to $lastSentId")
+    case RecoveryCompleted =>
+      log.debug("RecoveryCompleted")
   }
+
+  override def receiveCommand: Receive = loop(Nil)
 }
 
 case class Subscribe(actor: ActorRef)
 
 case class Unsubscribe(actor: ActorRef)
+
+object SenderId {
+  def uid = UUID.randomUUID().toString.take(10)
+}
+
+trait SenderId {
+  def uid: String
+
+  override def toString: String = uid
+}
+
+case class Envelope (sender: SenderId, event: Event, id: Long) {
+  override def toString: String = {
+    s"EventEnvelope[${id}](sender=$sender, event=$event)"
+  }
+}
