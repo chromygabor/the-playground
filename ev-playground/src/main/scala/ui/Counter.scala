8727a19087d2ab2dd9019283683e7b26edb400a7
Gabor Chromy <chromygabor@gmail.com>
2016-11-05 14:30:39 +0100

    Update Counter.scala

--- ev-playground/src/main/scala/ui/Counter.scala
+++ ev-playground/src/main/scala/ui/Counter.scala
@@ -1,10 +1,15 @@
 package ui
 
-import javafx.beans.property.SimpleStringProperty
-import javafx.beans.value.ObservableStringValue
 import javafx.event.ActionEvent
 import javafx.fxml.FXML
-import javafx.scene.control.{Label, Button}
+import javafx.scene.control.Label
+
+import akka.actor.ActorRef
+import ui.CounterService.{DecrementCounter, IncrementCounter}
+import ui.MyTask.{TaskFinished, TaskStarted}
+
+import scala.concurrent.Future
+import akka.pattern._
 
 /**
   * Created by chrogab on 2016.10.12..
@@ -13,46 +18,79 @@ case class CounterState()
 
 case object Increment extends Command
 case object Decrement extends Command
+case object Close extends Command
 
-class CounterBehavior extends Behavior[CounterState] {
+case class CounterChanged(value: Int) extends PersistentEvent
+
+
+object CounterBehavior {
+  val CounterTaskId = SimpleTaskId()
+}
+
+class CounterBehavior(initialValue: Int, counterService: ActorRef) extends Behavior[CounterState] {
 
   override def initialState: CounterState = CounterState()
 
-  override def onCommand(state: CounterState): PartialFunction[Command, List[Event]] = {
+  override def onCommand(state: CounterState): PartialFunction[Command, Seq[Event]] = {
+    case Init =>
+      CounterChanged(initialValue)
     case Increment =>
-      Nil
+      taskAs[Int](CounterBehavior.CounterTaskId, counterService ? IncrementCounter(behaviorId)).onSuccess { (state, value) =>
+        CounterChanged(value)
+      }.onFailure { (state, error ) =>
+        log.error("Error occurred during Increment: ", error)
+        error.printStackTrace()
+        Nil
+      }
     case Decrement =>
+      taskAs[Int](CounterBehavior.CounterTaskId, counterService ? DecrementCounter(behaviorId)).onSuccess { (state, value) =>
+        CounterChanged(value)
+      }.onFailure { (state, error ) =>
+        log.error("Error occurred during Decrement: ", error)
+        error.printStackTrace()
+        Nil
+      }
+    case Close =>
       Nil
     case e =>
-      log.info(s"Event received: $e")
+      log.info(s"Command received in CounterBehavior: $e")
       Nil
   }
 
-  override def onEvent(state: CounterState, context: AppContext): PartialFunction[Event, CounterState] = {
-    case e => state
+  override def onEvent(state: CounterState, context: BehaviorContext): PartialFunction[Event, CounterState] = {
+    case e =>
+      log.debug(s"Received: $e")
+      state
   }
 }
 
-
 class CounterController extends Controller {
 
-//  @FXML private var _btnDecrement: Button = _
-//  lazy val btnDecrement = _btnDecrement
   @FXML private var _lblCounter: Label = _
   lazy val lblCounter = _lblCounter.textProperty()
-//  @FXML private var _btnIncrement: Button = _
-//  lazy val btnIncrement = _btnIncrement
-//  @FXML private var _btnClose: Button = _
-//  lazy val btnClose = _btnClose
 
   def incrementClicked(event: ActionEvent): Unit =  behavior ! Increment
-  def decrementClicked(event: ActionEvent): Unit =  behavior ! Increment
-  def closeClicked(event: ActionEvent): Unit =  behavior ! Increment
+  def decrementClicked(event: ActionEvent): Unit =  behavior ! Decrement
+  def closeClicked(event: ActionEvent): Unit =  behavior ! Close
 
   override def onEvent: EventReceive = {
+    case TaskStarted(CounterBehavior.CounterTaskId) => {
+      log.debug("TaskStarted: Counter")
+      ui { lblCounter.set("...") }
+    }
+    case TaskFinished(CounterBehavior.CounterTaskId) => {
+      log.debug("TaskFinished: Counter")
+      ui()
+    }
+
     case Initialized =>
       ui {
         lblCounter.set("0")
+      }
+    case CounterChanged(value) =>
+      log.debug(s"CounterChanged: $value")
+      ui {
+        lblCounter.set(value.toString)
       }
   }
 }
