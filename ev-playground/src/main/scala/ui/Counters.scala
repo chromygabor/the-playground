b5238a46aac52f8c8d308bc0219b56dd3fdf7c14
Gabor Chromy <chromygabor@gmail.com>
2016-11-05 14:30:52 +0100

    Update Counters.scala

--- ev-playground/src/main/scala/ui/Counters.scala
+++ ev-playground/src/main/scala/ui/Counters.scala
@@ -5,7 +5,10 @@ import javafx.fxml.FXML
 import javafx.scene.control.Button
 import javafx.scene.layout.FlowPane
 
-import akka.actor.Props
+import akka.actor.{ActorRef, Props}
+
+import scala.concurrent.Future
+import scala.util.{Failure, Success}
 
 /**
   * Created by chrogab on 2016.10.12..
@@ -14,30 +17,49 @@ case class CountersState()
 
 case object AddCounter extends Command
 
-case class CounterAdded(behavior: BehaviorId = BehaviorId[CounterBehavior]) extends PersistentEvent
+case class CounterAdded(behavior: BehaviorId, initialValue: Int) extends PersistentEvent
 
-class CountersBehavior extends Behavior[CountersState] {
+object CountersBehavior {
+  val CountersTaskId = QueueTaskId()
+}
+
+/**
+  * Behavior
+  * @param counterService
+  */
+class CountersBehavior(counterService: ActorRef) extends Behavior[CountersState] {
+
+  import akka.pattern._
+
   override def initialState: CountersState = CountersState()
 
   override def onCommand(state: CountersState): PartialFunction[Command, List[Event]] = {
     case AddCounter =>
-      CounterService.behaviorId ? AddCounter
-      Nil
-
+      log.debug("AddCounter")
+      taskAs[(BehaviorId, Int)](CountersBehavior.CountersTaskId, counterService ? AddCounter)
+        .onSuccess { case (s, (behaviorId, initialValue)) =>
+          CounterAdded(behaviorId, initialValue)
+        }.onFailure { case (s, error) =>
+          Nil
+        }
+        Nil
     case Init =>
       Initialized
   }
 
-  override def onEvent(state: CountersState, context: AppContext): PartialFunction[Event, CountersState] = {
+  override def onEvent(state: CountersState, context: BehaviorContext): PartialFunction[Event, CountersState] = {
     case Initialized =>
       state
 
-    case CounterAdded(behaviorId) =>
-      context.create(behaviorId, new CounterBehavior)
+    case CounterAdded(behaviorId, initialValue) =>
+      context.create(behaviorId, Props(new CounterBehavior(initialValue, counterService)))
       state
   }
 }
 
+/**
+  * Controller
+  */
 class CountersController extends Controller {
 
   @FXML private var _bAdd: Button = _
@@ -48,10 +70,10 @@ class CountersController extends Controller {
 
   def pCounters = _pCounters
 
-  def addCounter(event: ActionEvent): Unit =  behavior ! AddCounter
+  def addCounter(event: ActionEvent): Unit = behavior ! AddCounter
 
   override def onEvent: EventReceive = {
-    case CounterAdded(behaviorId) =>
+    case CounterAdded(behaviorId, initialValue) =>
       ui {
         pCounters.getChildren.add(load[CounterController](behaviorId))
       }
